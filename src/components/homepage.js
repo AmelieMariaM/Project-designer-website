702303e0b9bd30e301784e3fcc3c12e46f5a19c0
amelie <ameliemasson78@gmail.com>
2024-07-19 14:25:49 +0200

    hamburger menu added for responsive website

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,7 +3,7 @@ import "../styles/homepage.css";
 
 function Homepage() {
   return (
-    <section>
+    <section className="homepage">
       <h1>Elliot Masson</h1>
       <h2>ILLUSTRATION & DESIGN</h2>
       <img id="image-accueil" src={homepage} alt="dessins portfolio accueil" />
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,39 +1,46 @@
+import React, { useState } from "react";
 import "../styles/nav.css";
 import logo from "../assets/logo 100px-02.png";
 
 function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <nav class="main-nav">
-      <div>
-        <img src={logo} alt="logo-Elliot" class="logo" />
-      </div>
-      <ul class="nav-list">
+    <nav className="navbar">
+      <img src={logo} alt="logo-Elliot" className="logo" />
+      <ul className={`nav-list ${isOpen ? "open" : ""}`}>
         <li>
-          <a class="nav-link" href="#home">
+          <a className="nav-link" href="#home">
             HOME
           </a>
         </li>
         <li>
-          <a class="nav-link" href="#about">
+          <a className="nav-link" href="#about">
             ABOUT
           </a>
         </li>
         <li>
-          <a class="nav-link" href="#design">
+          <a className="nav-link" href="#design">
             DESIGN
           </a>
         </li>
         <li>
-          <a class="nav-link" href="#art">
+          <a className="nav-link" href="#art">
             ILLUSTRATIONS & ART
           </a>
         </li>
         <li>
-          <a class="nav-link" href="#contact">
+          <a className="nav-link" href="#contact">
             CONTACT
           </a>
         </li>
       </ul>
+      <button className="navbar-burger" onClick={toggleMenu}>
+        <span className="burger-bar"></span>
+      </button>
     </nav>
   );
 }
